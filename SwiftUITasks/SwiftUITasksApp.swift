ea6dd63094237f1af16b68e4fa953b902d5638e1
Ruslan <ruslan99992222@gmail.com>
2024-04-29 12:32:34 +0300

    Initial Commit

---
+++ SwiftUITasks/SwiftUITasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITasksApp.swift
+//  SwiftUITasks
+//
+//  Created by Руслан Абрамов on 29.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
